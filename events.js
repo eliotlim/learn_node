75cfc965f471d6a9b97be9401db37e39d75aa5fe
Eliot Lim <eliot.lim@gmail.com>
2017-07-27 15:46:16 +0800

    console.log on server connectHandle
    + Server connection handling fires `connection` event

--- events.js
+++ events.js
@@ -20,7 +20,24 @@ eventEmitter.on('data_received', function(){
     console.log("data received successfully.");
 });
 
-// Fire the connection event
-eventEmitter.emit('connection');
+console.log("Event Fired");
 
-console.log("Program Ended.");
+var http = require("http");
+
+var server = http.createServer(function (request, response) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type':'text/plain'});
+
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+
+    // Fire the connection event
+    eventEmitter.emit('connection');
+});
+
+server.listen(8081);
+
+// Console will log the message
+console.log("Event Server running at http://localhost:8081");
