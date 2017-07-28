93edc7f3e87a0c5b9029e32ea0d713fdb23b973f
Eliot Lim <eliot.lim@gmail.com>
2017-07-28 15:16:49 +0800

    Read Input File
    + Read `input.txt`
    + Output through `console.log`

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-console.log("Hello World");
+var fs = require("fs");
 
-var http = require("http");
-
-var server = http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type':'text/plain'});
-
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
+fs.readFile("input.txt", function(err, data) {
+    if (err) {
+        console.log(err.stack);
+        return;
+    }
+    console.log(data.toString());
 });
 
-server.listen(8081);
-
-// Console will log the message
-console.log("Server running at http://localhost:8081");
+console.log("Program Ended.");
